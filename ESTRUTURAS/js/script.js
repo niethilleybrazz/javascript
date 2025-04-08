1264acc08614fe43b368bd92f06e477ca0a94afd
Ni Braz <niethilley@gmail.com>
2025-04-08 16:41:32 -0300

    Js. Atualizado

--- ESTRUTURAS/js/script.js
+++ ESTRUTURAS/js/script.js
@@ -1,22 +1,22 @@
-// 1 - Variaveis
-let nome = "Matheus" //let pode ser mudado 
+// // 1 - Variaveis
+// let nome = "Matheus" //let pode ser mudado 
 
-console.log(nome)
+// console.log(nome)
 
-nome = "Mariana"
+// nome = "Mariana"
 
-console.log(nome)
+// console.log(nome)
 
-const idade = 15 // const o valor nao pode ser alterado, sendo o mais utilizado
+// const idade = 15 // const o valor nao pode ser alterado, sendo o mais utilizado
 
-console.log(idade)
+// console.log(idade)
 
-// 2 - Mais sobre variaveis 
-let a = 10, b = 20, c = 30
-console.log(a, b,c)
+// // 2 - Mais sobre variaveis 
+// let a = 10, b = 20, c = 30
+// console.log(a, b,c)
 
-const nomeCompleto = "Matheus"
-console.log(nomeCompleto)
+// const nomeCompleto = "Matheus"
+// console.log(nomeCompleto)
 
 // 3 - Funcoes de Prompt
 // const nomeProprio = prompt("Digite seu nome")
@@ -24,6 +24,34 @@ console.log(nomeCompleto)
 // console.log(`Seu nome e ${nomeProprio}`)
 
 // 4 - Funcao de Alert
-alert("Testando")
+// alert("Testando")
 
-// 5 - 
+// 5 - Math 
+// console.log(Math.max(1,4,7,9)) 
+
+// 6 - Console
+// console.error("Opa!")
+// console.warn("Uepa")
+
+// 7 - If 
+const user = "Joao"
+if(user === "Joao"){
+    console.log("Bem vindo Joao")
+}
+
+// 8 - Else 
+const nota = 9
+if(nota >= 6){
+    console.log("Passou")
+} else {
+    console.log("Nao passou")
+}
+
+// 9 - Else If
+if (1 > 2) {
+    console.log("Teste")
+} else if (2 > 3) {
+    console.log("Teste 2")
+} else if (5 > 1) {
+    console.log("Agora sim!")
+} 
