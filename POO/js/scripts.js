51ff26bd87ddc601927b02784dd90bec706eb04b
Ni Braz <niethilley@gmail.com>
2025-05-26 15:28:45 -0300

    Metodos, Prototypes e Classes

--- POO/js/scripts.js
+++ POO/js/scripts.js
@@ -1,4 +1,53 @@
 // 1 - metodo
 const animal = {
-    nome:"Bob";
+    nome:"Bob",
+    latir: function() {
+        console.log("Au Au")
+    }
 }
+console.log(animal.nome)
+
+animal.latir()
+
+// 2 - aprofundando em metodos
+
+const pessoa = {
+    nome:"Matheus",
+    getNome: function () {
+        return this.nome
+    },
+
+    setNome: function (novoNome) {
+        this.nome = novoNome
+    }
+}
+
+console.log(pessoa.nome)
+console.log(pessoa.getNome())
+
+pessoa.setNome("Joaquim")
+
+console.log(pessoa.getNome()) 
+
+// 3 - prototypes
+const text = "asd"
+
+console.log(Object.getPrototypeOf(text))
+
+const bool = true 
+
+console.log(Object.getPrototypeOf(bool))
+
+// 4 - classes basicas 
+const cachorro = {
+    raca: null,
+    patas: 4
+}
+
+const pastorAlemao = Object.create(cachorro)
+
+pastorAlemao.raca = "Pastor Alemao"
+
+console.log(pastorAlemao)
+console.log(pastorAlemao.patas)
+console.log(cachorro)
