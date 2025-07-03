0afa6b56ce1cec12e126413f1dd87d75f0f04cc3
Ni Braz <niethilley@gmail.com>
2025-07-03 10:13:55 -0300

    att

---
+++ DEBUG/js/scripts.js
@@ -0,0 +1,40 @@
+// 1 - strict 
+"use strict"
+
+const opa = "teste"
+
+// 2 - console.log
+let a = 1
+let b = 2 
+
+if (a == 1) {
+    a = b+2
+}
+
+console.log(a);
+
+for (let i = 0; i< b ; i++) {
+    a = a + 2 
+    console.log(a);
+}
+
+if (a>5) {
+    a = 25
+}
+console.log(a);
+
+// 3 - debugger
+let c = 1 
+let d = 2
+
+if (c==1) {
+    c = d + 2 
+}
+
+debugger
+
+for (let i = 0; i < d; i++) {
+   c = c + 2 
+}
+
+// 4 -Tratamento de dado
