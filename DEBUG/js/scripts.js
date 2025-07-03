a189b4b4137612b1ffa5d29afd2f5c8d368f5993
Ni Braz <niethilley@gmail.com>
2025-07-03 15:15:25 -0300

    Update

--- DEBUG/js/scripts.js
+++ DEBUG/js/scripts.js
@@ -31,10 +31,63 @@ if (c==1) {
     c = d + 2 
 }
 
-debugger
+// debugger
 
 for (let i = 0; i < d; i++) {
    c = c + 2 
 }
 
-// 4 -Tratamento de dado
+// 4 -Tratamento de dados
+function checkNumber(n) {
+    const result = Number(n)
+
+    if (Number.isNaN(result)) {
+        console.log("Erro")
+    }
+    console.log("Bem vindo")
+    return result
+}
+
+checkNumber(7)
+
+// 5 - Execptions
+let x = 10 
+
+// if (x != 11) {
+//     throw new Error("Opa! Produto indisponivel");
+// }
+
+// 6 - try catch
+    try {
+        const soma = x + y
+    } catch (error) {
+        console.log(`Erro no programa ${error}`)
+    }
+
+// 7 - finally
+
+    try {
+        const value = checkNumber("asd")
+        
+        if (!value) {
+            throw new Error("Valores invalidos");
+        }
+        
+    }catch(error){
+        console.log(`Opa! Deu erro ai : ${error}`);
+    } finally {
+        console.log("The code was executed");
+        
+    }
+
+// 8 - assertions 
+    
+    function checkArray(arr) {
+        if (arr.length === 0) {
+            throw new Error("Uai, tem nada aqui");
+        } else {
+            console.log(`O array tem ${arr.length} elementos agora`);
+        }
+    }
+
+    checkArray([1,2,3])
