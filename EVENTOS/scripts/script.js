4aaf5e338602e41404d3ec1544eff27b5bf43f79
Niethilley Braz <niethilley@gmail.com>
2025-07-21 12:17:08 -0300

    Atualizado

--- EVENTOS/scripts/script.js
+++ EVENTOS/scripts/script.js
@@ -14,3 +14,50 @@ const myTitle = document.querySelector("#my-title")
 myTitle.addEventListener("click", (event)=>{
     console.log(event);
 })
+
+// 3 - eventos de tecla 
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`);
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Presionou a tecla ${e.key}`);
+})
+
+// 4 - eventos de mouse 
+const mouse = document.querySelector("#mouse")
+
+mouse.addEventListener("mousedown", () => {
+    console.log("Presionou!");
+})
+
+// 5 - movimento do mouse 
+document.addEventListener("mousemove", (e) => {
+    // console.log(`no eixo X ${e.x}`)
+    // console.log(`no eixo Y ${e.y}`)
+})
+
+// 6 - evento de scroll 
+window.addEventListener("scroll", (e)=>{
+    // if (window.pageYOffset > 200) {
+    //     console.log("Passamos de 200px");   
+    // }
+})
+
+// 7 - eventos de foco/blur
+const input = document.querySelector("#myInput")
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no input");
+    input.style.backgroundColor = "#e4e4e4ff"
+})
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu no input");
+    input.style.backgroundColor = "#b6b6b6ff"
+})
+
+// 8 - evento de carregamento 
+window.addEventListener("load", (e)=>{
+    console.log("A pagina carregou");
+})
+    
