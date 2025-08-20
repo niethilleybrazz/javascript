daa13ec9d1be891bcc66972a5381c881103cbcc3
Niethilley Braz <niethilley@gmail.com>
2025-08-20 19:22:01 -0300

    att

---
+++ AXIOS/js/scripts.js
@@ -0,0 +1,39 @@
+// 1 - Instalacao 
+console.log(axios);
+
+// 2 - primeiro request
+const getData = async () => {
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+
+        console.log(response);  
+
+        return response.data
+    } catch (error) {
+        console.log(error);
+    }
+}
+getData()   
+
+// 3 - imprimindo dados na tela
+const container = document.querySelector("#user-container")
+
+const printData = async () => {
+    const data = await getData()
+
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("ul") // Em forma de lista (na aula estava como div)
+        const nameElement = document.createElement("li") // Em forma de lista (na aula estava como h2)
+        nameElement.textContent = user.name 
+
+        const emailElement = document.createElement("p")
+        emailElement.textContent = user.email
+
+        div.appendChild(emailElement)
+        div.appendChild(nameElement)
+        container.appendChild(div)
+    });
+}
+printData()
