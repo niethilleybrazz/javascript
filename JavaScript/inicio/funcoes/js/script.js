a13dcc79f4b713ba8331dfba88be1589c282578b
Niethilley Braz <niethilley@gmail.com>
2026-03-03 14:03:30 -0300

    feat: add initial HTML and JavaScript files for data types, operators, and OOP concepts
    - Created index.html and script.js for JavaScript operators demonstration
    - Added HTML structure and basic JavaScript operations in script.js
    - Implemented object-oriented programming examples in a new directory
    - Developed exercises for number manipulation and form handling
    - Included examples of array manipulation and function usage
    - Established object creation and method demonstration in JavaScript

---
+++ JavaScript/inicio/funcoes/js/script.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome: "Luiz",
+    sobrenome: "Fuggini",
+    idade : 30, 
+    endereco:{
+        rua:"Takamassa Nomuro", 
+        numero:67,
+        bairro:"Nagasaki"
+    }
+}
+
+const {nome} = pessoa
+console.log(nome);
