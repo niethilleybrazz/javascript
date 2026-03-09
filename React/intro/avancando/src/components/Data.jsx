825ab5f1806498e3511d0445a4a273bee1161887
Niethilley Braz <niethilley@gmail.com>
2026-03-09 16:46:49 -0300

    feat: add Data component with state management and variable update functionality

---
+++ React/intro/avancando/src/components/Data.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const Data = () => {
+    let someData = 10
+    const[anotherNumber, setAnotherNumber] = useState(0)
+
+  return (
+    <div>
+        <div>
+            <p>Valor: {someData}</p>
+            <button onClick={()=> someData = 15}>Mudar Variavel</button>
+        </div>
+        <div>
+            <p>Valor: {anotherNumber}</p>
+            <button onClick={()=> setAnotherNumber(anotherNumber + 1)}>Mudar State</button>
+        </div>
+    </div>
+  )
+}
+
+export default Data
